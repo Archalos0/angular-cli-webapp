c096a25c487ba129805044fecdb5d219a232abd3
TifSP <44550222+TifSP@users.noreply.github.com>
2022-03-03 23:13:01 +0100

    Add files via upload

---
+++ protractor.conf.js
@@ -0,0 +1,21 @@
+exports.config = {
+  directConnect: !process.env.IS_JENKINS,
+   capabilities: {
+     browserName: 'chrome'
+   },
+   baseUrl: 'http://localhost:8080',
+   specs: ['e2e/*.spec.ts'],
+   onPrepare: () => {
+  if (process.env.IS_JENKINS) {
+       let jasmineReporters = require('jasmine-reporters');
+       let junitReporter = new jasmineReporters.JUnitXmlReporter({
+         savePath: 'output/',
+         consolidateAll: false
+       });
+       jasmine.getEnv().addReporter(junitReporter);
+     }
+     require('ts-node').register({
+       project: 'e2e'
+     });
+   },
+ };
