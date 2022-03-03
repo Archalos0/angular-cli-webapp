ec3186f4db20440198cdb6e0989670d700531043
TifSP <44550222+TifSP@users.noreply.github.com>
2022-03-03 23:14:15 +0100

    Add files via upload

---
+++ e2e/test.spec.js
@@ -0,0 +1,12 @@
+describe('divers tests web', function() {
+
+  it('should good view data of BDD', function() {
+    browser.get('http://localhost:8080');
+
+browser.driver.findElement(by.id('customers'));
+expect('donnée web').toEqual('donnée de la bdd');
+
+browser.driver.findElement(by.xpath('//id="customer"'));
+expect('donnée web').toEqual('donnée de la bdd');
+  });
+});
